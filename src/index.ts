f5028da3a227ae894275021114896b59434a3efc
Arnaud Barré <arnaud.barre@carbometrix.com>
2025-05-18 01:09:27 +0200

    Provide default filePath

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import type { Parser, Plugin } from "prettier";
 import { oxcParse } from "./oxcParse.ts";
 
-const parser: Parser = {
+const parser = (defaultFile: string): Parser => ({
   astFormat: "estree",
-  parse: (text, options) => oxcParse(text, options.filepath),
+  parse: (text, options) => oxcParse(text, options.filepath ?? defaultFile),
   locStart: (node) => node.start,
   locEnd: (node) => node.end,
   // The default plugins do a lot of work based on parsing through jest-docblock that does quite
   // a lot of regexes. Not sure if it's worth it to do that here
   hasPragma: (text) => text.includes("@format") || text.includes("@prettier"),
-};
+});
 
 export const parsers: Plugin["parsers"] = {
-  babel: parser,
-  typescript: parser,
+  babel: parser("file.js"),
+  typescript: parser("file.ts"),
 };
